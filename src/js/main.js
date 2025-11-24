e385972a4d50b87cc208985a3e67baacc74032c9
Luis Rodrigues <luismiguelfr74@hotmail.com>
2025-11-24 18:53:21 +0000

    Prettier.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-'option explicit';
+"option explicit";
 
 function createPage(title, prefix, n) {
-    if (title) createMenuItems(title);
-    if (prefix) createGaleryItems(title, prefix, n);
+  if (title) createMenuItems(title);
+  if (prefix) createGaleryItems(title, prefix, n);
 }
 
 function createMenuItems(title) {
@@ -43,7 +43,7 @@ function createMenuItems(title) {
   ];
 
   let html = "";
-  navBarData.forEach(d => {
+  navBarData.forEach((d) => {
     d.title = d.id;
     const cls = title === d.title ? "active" : "";
     html += `<li class="nav-item"><a class="nav-link ${cls}" href="${d.href}">${d.title}</a></li>`;
@@ -59,17 +59,17 @@ function createGaleryItems(title, prefix, n) {
   for (i = 1; i <= n; i++) {
     const imageData = {
       href: `images/${title}/${prefix} (${i}).jpg`,
-      alt: `${prefix} #${i}`
+      alt: `${prefix} #${i}`,
     };
 
     galeryData.push(imageData);
   }
 
   let html = "";
-  galeryData.forEach(d => {
+  galeryData.forEach((d) => {
     html += `<div class="col d-flex align-items-center"><img class="img-fluid mx-auto" src="${d.href}" alt="${d.alt}" /></div>`;
   });
-  
+
   const navbarItemsElement = document.getElementById("pageGalery");
   navbarItemsElement.innerHTML = html;
 }
